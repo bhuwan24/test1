036fc1670c147b7dd9abdf6267b0d7a889dd6812
manish <tiwari.manish409@gmail.com>
2018-06-19 09:58:55 +0530

    Commit In Second Branch

---
+++ rev_SecondBranchfile.c
@@ -0,0 +1,28 @@
+//This is my file
+
+#include <stdio.h>
+#include <string.h> 
+
+main()
+{
+int l,i;
+char str[100];
+printf("enter the string\n");
+gets(str);
+l=strlen(str);
+for(i=l-1;i>=0;i--)
+printf("%c",*(str+i));
+printf("\n");
+printf("Hello...with changes\n");
+Printf("Hope you are enjoying...you are in scond branch.......\n");
+    }
+
+
+
+
+
+
+
+
+
+
